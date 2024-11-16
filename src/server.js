a5f595868202e1298cf03cfa96439f42a06be321
gabrielrocha-youdb <gabriel.rocha@youdb.com.br>
2024-11-16 19:00:02 -0300

    fix: trocando a porta para o defalt

--- src/server.js
+++ src/server.js
@@ -63,5 +63,5 @@ app.get('/projetos', async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 3333;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
